4cd96bdf6a0f683dc49b09a90db5ba9ef92fcdec
mmwihaki <marymwihaki16@gmail.com>
2022-10-27 20:37:04 +0300

    add buttons to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import LinkButton from './components/LinkButton'
 function App() {
   return (
     <div>
-      <LinkButton name='Follow me on Instagram' />
+      <LinkButton name='Follow me on Instagram'link='https://instagram.com/maxthepalacehorse'/>
+      <LinkButton name='Follow me on TikTok' link='#' />
+      <LinkButton name='Follow my art on Instagram' link='#' />
+      <LinkButton name='Web Dev Portfolio' link='#' />
+      <LinkButton name='Checkout my Codecast Profile' link='#' />
     </div>
   )
 }
--- src/components/textbanner.js
+++ src/components/textbanner.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const TextBanner = 0 => {
 
+    const {
+        title, 
+        text
+    } = props
+
     return (
-        
+        <div>
+            <h1>{title}</h1>
+            <p>{text}</p>
+        </div>
     )
 }
 
